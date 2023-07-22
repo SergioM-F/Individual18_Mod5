cb4483f3466b483b24e9cfe245ff23da939c5173
Sergio <sergiomolinaflores1@gmail.com>
2023-07-21 20:34:02 -0400

    binding del boton guardar

--- app/src/main/java/cl/samf/individual18_mod5/MainActivity.kt
+++ app/src/main/java/cl/samf/individual18_mod5/MainActivity.kt
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cl.samf.individual18_mod5.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonGUARDAR.setOnClickListener {
+            val texto = binding.textViewTexto.toString()
+            val entero = binding.editTextNumEntero.toString().toInt()
+            val decimal = binding.editTextNumberDecimal.toString().toDouble()
+            val switch = binding.switchFragmen.isChecked
+        }
+
     }
 }
