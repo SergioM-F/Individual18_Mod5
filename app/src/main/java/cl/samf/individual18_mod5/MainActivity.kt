1143c548aea47de3e4a821e2ec9662f72a1ac13e
Sergio <sergiomolinaflores1@gmail.com>
2023-07-21 22:00:06 -0400

    Individual 18 Ok

--- app/src/main/java/cl/samf/individual18_mod5/MainActivity.kt
+++ app/src/main/java/cl/samf/individual18_mod5/MainActivity.kt
@@ -1,23 +1,73 @@
 package cl.samf.individual18_mod5
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import cl.samf.individual18_mod5.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mSharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        mSharedPreferences = getSharedPreferences("valoresApp", Context.MODE_PRIVATE)
 
         binding.buttonGUARDAR.setOnClickListener {
-            val texto = binding.textViewTexto.toString()
-            val entero = binding.editTextNumEntero.toString().toInt()
-            val decimal = binding.editTextNumberDecimal.toString().toDouble()
-            val switch = binding.switchFragmen.isChecked
+            val texto = binding.editTextTexto.text.toString()
+            val entero = binding.editTextNumEntero.text.toString().toInt()
+            val decimal = binding.editTextNumberDecimal.text.toString().toFloat()
+            val boleano = binding.switchFragmen.isChecked
+
+            guardarDatos(texto,entero,decimal,boleano)
+        }
+
+        binding.buttonMostrar.setOnClickListener {
+            mostrarDatos()
+        }
+
+        binding.buttonBorrar.setOnClickListener {
+            borrarDatos()
         }
 
     }
+
+    private fun guardarDatos(texto: String, entero: Int, decimal: Float, boleano: Boolean){
+        mSharedPreferences.edit().putString("miTexto", texto).apply()
+        mSharedPreferences.edit().putInt("miEntero", entero).apply()
+        mSharedPreferences.edit().putFloat("miDecimal", decimal).apply()
+        mSharedPreferences.edit().putBoolean("miBoleano", boleano).apply()
+    }
+    private fun mostrarDatos() {
+        val texto = mSharedPreferences.getString("miTexto","")
+        val entero = mSharedPreferences.getInt("miEntero",0)
+        val decimal = mSharedPreferences.getFloat("miDecimal", 0.0f)
+        val boleano = mSharedPreferences.getBoolean("miBoleano", false)
+
+        binding.textViewTexto.text = texto
+        binding.textViewrEntero.text = entero.toString()
+        binding.textViewDecimal.text = decimal.toString()
+        binding.textViewSwitch.text = boleano.toString()
+        binding.switchFragmen.isChecked = boleano
+
+
+    }
+    private fun borrarDatos() {
+        binding.textViewTexto.text = ""
+        binding.textViewrEntero.text = ""
+        binding.textViewDecimal.text = ""
+        binding.textViewSwitch.text = ""
+
+        binding.editTextTexto.text.clear()
+        binding.editTextNumEntero.text
+        binding.editTextNumberDecimal.text
+        binding.switchFragmen.isChecked = false
+        mSharedPreferences.edit().clear()
+
+    }
+
 }
